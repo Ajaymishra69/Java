6b29330ef0a2de3b86ecbd86975c0b215efc6c54
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-04-04 08:32:27 +0530

    Add files via upload

---
+++ P3_Task01_VariablesAndDataTypesInJava.java
@@ -0,0 +1,65 @@
+public class P3_Task01_VariablesAndDataTypesInJava {
+
+
+static int q = 11;
+int p = 10;
+
+public static void main(String[] args) {
+
+//Valid declartions
+int a,b,c;
+float pi;
+double d;
+char e;
+
+//Valid Initializations
+pi = 3.14f;
+d = 20.22d;
+e ='v';
+a = 10;
+b = 10;
+c = 10;
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+System.out.println(pi);
+System.out.println(d);
+System.out.println(e);
+
+int myNum = 5;  //Integer {whole number}
+float myFloatNum = 5.99f; //Floating point number
+char myLetter = 'D'; // Character
+boolean myBool = true; //Boolean
+String myText = "Hello"; //String
+
+System.out.println(myNum);
+System.out.println(myFloatNum);
+System.out.println(myLetter);
+System.out.println(myBool);
+System.out.println(myText);
+double f;
+int i=10;
+f=i;//type conversion
+double g=10;
+int j;
+j=(int)g;//type casting
+System.out.println(f);
+System.out.println(g);
+System.out.println(j);
+System.out.println(i);
+byte k =10;
+boolean l=true;
+long m=10L;
+float n=1.2f;
+double x=1.2d;
+System.out.println(k);
+System.out.println(l);
+System.out.println(m);
+System.out.println(n);
+System.out.println(x);
+
+
+}
+};
